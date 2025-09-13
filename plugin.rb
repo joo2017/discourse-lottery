56069e1f73856583ea00fcf4aa0b07eb5869e9bd
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 00:14:27 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,96 +1,43 @@
 # frozen_string_literal: true
-
 # name: discourse-lottery
-# about: Adds the ability to create lottery activities in topics
+# about: A lottery plugin for Discourse based on discourse-calendar
 # version: 1.0
-# author: Based on discourse-calendar
-# url: https://github.com/discourse/discourse-lottery
+# authors: Your Name
+# url: https://github.com/your-repo/discourse-lottery
 
-enabled_site_setting :lottery_enabled
+PLUGIN_NAME = "discourse-lottery"
 
-# 注册前端资源
-register_asset "stylesheets/common/discourse-post-lottery.scss"
-register_asset "stylesheets/common/discourse-post-lottery-preview.scss"
-register_asset "stylesheets/mobile/discourse-post-lottery.scss", :mobile
-register_asset "stylesheets/colors.scss", :color_definitions
+enabled_site_setting :discourse_lottery_enabled
 
-register_svg_icon "gift"
-register_svg_icon "trophy"
-register_svg_icon "dice"
-register_svg_icon "users"
-register_module("ember-math-helpers", "ember-math-helpers/dist/modules/helpers.js", type: :es6)
+# 注册资源路径
+register_asset "stylesheets/common/discourse-lottery.scss"
+register_asset "stylesheets/desktop/discourse-lottery.scss", :desktop
+register_asset "stylesheets/mobile/discourse-lottery.scss", :mobile
 
-module ::DiscoursePostLottery
-  PLUGIN_NAME = "discourse-post-lottery"
-  
-  TOPIC_POST_LOTTERY_STARTS_AT = "TopicLotteryStartsAt"  
-  TOPIC_POST_LOTTERY_ENDS_AT = "TopicLotteryEndsAt"
-end
-
-require_relative "lib/discourse_post_lottery/engine"
+# 加载引擎
+load File.expand_path("lib/discourse_lottery/engine.rb", __dir__)
 
 after_initialize do
-  # 加载模型扩展
-  require_relative "lib/discourse_post_lottery/post_extension"
-
-  # 前端需要的扩展
-  reloadable_patch do
-    Post.prepend(DiscoursePostLottery::PostExtension)
+  # 加载所有扩展模块
+  require_relative "lib/discourse_lottery/user_extension"
+  require_relative "lib/discourse_lottery/post_extension"
+  
+  # 扩展核心模型
+  User.class_eval do
+    include DiscourseLottery::UserExtension
   end
-
-  # 用户权限
-  add_to_class(:user, :can_create_discourse_post_lottery?) do
-    return @can_create_discourse_post_lottery if defined?(@can_create_discourse_post_lottery)
-    @can_create_discourse_post_lottery =
-      begin
-        return true if staff?
-        allowed_groups = SiteSetting.lottery_allowed_on_groups.to_s.split("|").compact
-        allowed_groups.present? &&
-          (
-            allowed_groups.include?(Group::AUTO_GROUPS[:everyone].to_s) ||
-              groups.where(id: allowed_groups).exists?
-          )
-      rescue StandardError
-        false
-      end
+  
+  Post.class_eval do
+    prepend DiscourseLottery::PostExtension
   end
-
-  add_to_class(:guardian, :can_create_discourse_post_lottery?) do
-    user && user.can_create_discourse_post_lottery?
+  
+  # 添加序列化器字段
+  add_to_serializer(:current_user, :can_create_discourse_lottery) do
+    object.can_create_discourse_lottery?
   end
-
-  add_to_class(:guardian, :can_act_on_discourse_post_lottery?) do |lottery|
-    user && user.staff? || (user && Guardian.new(user).can_edit_post?(lottery.post))
+  
+  # 注册路由
+  Discourse::Application.routes.draw do
+    mount ::DiscourseLottery::Engine, at: "/"
   end
-
-  add_to_serializer(:current_user, :can_create_discourse_post_lottery) do
-    object.can_create_discourse_post_lottery?
-  end
-
-  # 前端需要的序列化
-  add_to_serializer(
-    :post,
-    :lottery,
-    include_condition: -> do
-      SiteSetting.lottery_enabled && !object.nil? && !object.deleted_at.present?
-    end,
-  ) { object.lottery ? DiscoursePostLottery::LotterySerializer.new(object.lottery, scope: scope, root: false) : nil }
-
-  # Topic自定义字段（前端显示需要）
-  add_preloaded_topic_list_custom_field DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT
-
-  add_to_class(:topic, :lottery_starts_at) do
-    @lottery_starts_at ||= custom_fields[DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT]
-  end
-
-  add_to_serializer(
-    :topic_list_item,
-    :lottery_starts_at,
-    include_condition: -> do
-      SiteSetting.lottery_enabled && object.lottery_starts_at
-    end,
-  ) { object.lottery_starts_at }
-
-  # 注册自定义字段
-  register_post_custom_field_type("discourse_post_lottery", :string)
 end
