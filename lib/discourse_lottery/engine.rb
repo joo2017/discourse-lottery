f257f5660fca3d0000adcd03f9df17aa72f692cc
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 00:46:15 +0900

    Create engine.rb

---
+++ lib/discourse_lottery/engine.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module DiscourseLottery
+  PLUGIN_NAME = "discourse-lottery"
+  
+  class Engine < ::Rails::Engine
+    engine_name PLUGIN_NAME
+    isolate_namespace DiscourseLottery
+  end
+end
