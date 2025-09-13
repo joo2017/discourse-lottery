552c43144b048a6732e6e18cde20e354b23aa211
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 00:36:56 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,14 +9,14 @@ PLUGIN_NAME = "discourse-lottery"
 
 enabled_site_setting :discourse_lottery_enabled
 
-# 注册资源路径
+# 注册资源
 register_asset "stylesheets/common/discourse-lottery.scss"
 
-# 加载引擎
+# 加载引擎 - 这行必须在after_initialize之前
 load File.expand_path("lib/discourse_lottery/engine.rb", __dir__)
 
 after_initialize do
-  # 加载扩展模块（只有在文件存在时才加载）
+  # 加载扩展模块
   require_relative "lib/discourse_lottery/user_extension" if File.exist?(File.expand_path("lib/discourse_lottery/user_extension.rb", __dir__))
   
   # 扩展User模型
@@ -27,10 +27,5 @@ after_initialize do
   # 添加序列化器字段
   add_to_serializer(:current_user, :can_create_discourse_lottery) do
     object.respond_to?(:can_create_discourse_lottery?) ? object.can_create_discourse_lottery? : false
-  end
-  
-  # 注册路由
-  Discourse::Application.routes.draw do
-    mount ::DiscourseLottery::Engine, at: "/"
   end
 end
