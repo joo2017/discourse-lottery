bd50e3dd6002e0a5c367b10fcdcf36f49afd7842
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 00:06:25 +0900

    Update post_extension.rb

--- lib/discourse_post_lottery/post_extension.rb
+++ lib/discourse_post_lottery/post_extension.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-module DiscoursePostLottery
-  module PostExtension
-    extend ActiveSupport::Concern
-
-    def lottery
-      @lottery ||= DiscoursePostLottery::Lottery.find_by(id: id)
-    end
-
-    def lottery=(lottery_instance)
-      @lottery = lottery_instance
+module DiscourseLottery
+  module UserExtension
+    def can_create_discourse_lottery?
+      return false unless SiteSetting.discourse_lottery_enabled
+      
+      if SiteSetting.discourse_lottery_allowed_on_groups.present?
+        allowed_groups = SiteSetting.discourse_lottery_allowed_on_groups.split("|")
+        return self.groups.where(name: allowed_groups).exists?
+      end
+      
+      true
     end
   end
 end
