ace05872ae38a00eab9d9ff80335850bceb47d3b
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-15 15:16:17 +0900

    Update and rename 20250110120002_create_discourse_lottery_participants.rb to 20250916120001_create_discourse_lottery_participants.rb

--- db/migrate/20250916120001_create_discourse_lottery_participants.rb
+++ db/migrate/20250916120001_create_discourse_lottery_participants.rb
@@ -4,17 +4,12 @@ class CreateDiscourseLotteryParticipants < ActiveRecord::Migration[7.0]
   def change
     create_table :discourse_lottery_participants do |t|
       t.references :lottery, null: false, foreign_key: { to_table: :discourse_lottery_lotteries }
-      t.integer :user_id, null: false
-      t.integer :post_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.integer :post_number, null: false
-      t.datetime :participated_at, null: false
-
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :discourse_lottery_participants, [:lottery_id, :user_id], unique: true
     add_index :discourse_lottery_participants, :user_id
-    add_index :discourse_lottery_participants, :post_id
-    add_index :discourse_lottery_participants, :participated_at
   end
 end
