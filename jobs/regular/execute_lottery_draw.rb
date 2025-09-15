e35559227ca3cc40f493feff81967301bcd296a8
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-15 15:11:54 +0900

    Update execute_lottery_draw.rb

--- jobs/regular/execute_lottery_draw.rb
+++ jobs/regular/execute_lottery_draw.rb
@@ -4,7 +4,7 @@ module Jobs
   class ExecuteLotteryDraw < ::Jobs::Base
     def execute(args)
       lottery_id = args[:lottery_id]
-      raise Discourse::InvalidParameters.new(:lottery_id) unless lottery_id
+      return unless lottery_id
 
       DiscourseLottery::LotteryManager.execute_draw(lottery_id)
     end
