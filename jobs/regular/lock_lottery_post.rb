d045634e86124d6d9c2d36b4b4100ae8e341057c
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-15 15:12:11 +0900

    Update lock_lottery_post.rb

--- jobs/regular/lock_lottery_post.rb
+++ jobs/regular/lock_lottery_post.rb
@@ -4,15 +4,12 @@ module Jobs
   class LockLotteryPost < ::Jobs::Base
     def execute(args)
       post_id = args[:post_id]
-      raise Discourse::InvalidParameters.new(:post_id) unless post_id
+      return unless post_id
 
       post = Post.find_by(id: post_id)
-      return unless post&.lottery&.running?
+      return unless post&.lottery
 
-      lock_delay = SiteSetting.lottery_post_lock_delay_minutes.minutes
-      if post.created_at + lock_delay <= Time.zone.now
-        post.update_columns(locked: true)
-      end
+      post.update_columns(locked: true)
     end
   end
 end
